de961adaa4c6b4f0a8d67aed8c1b9a9d24b6296f
Mohit <mohitmajhi99@gmail.com>
2024-01-28 16:17:02 +0530

    The tickets have  been made for the cities and can display data for specific place the player is in uno also works but not chance

--- logic2.js
+++ logic2.js
@@ -7,6 +7,9 @@ const app = express();
 const port = 80;
 const IP_ADDRESS = '127.0.0.1';
 
+let previous_dice_sum1=1;
+let previous_dice_sum2=1;
+
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -17,10 +20,11 @@ app.get('/rollDice1', (req, res) => {
 
     // Update the result in the HTML file
     const result = `Dice 1: ${randomNum1} \n Dice 2: ${randomNum2}`;
+    previous_dice_sum1 = randomNum1+randomNum2;
     res.send(result);
 
     // Update the position in the JSON file
-    updatePosition('player1.json', randomNum1 + randomNum2);
+    updatePlayerData('player1.json', randomNum1 + randomNum2);
 });
 
 // Route to handle the roll request for Player 2
@@ -30,11 +34,20 @@ app.get('/rollDice2', (req, res) => {
 
     // Update the result in the HTML file
     const result = `Dice 1: ${randomNum1} \n Dice 2: ${randomNum2}`;
+    previous_dice_sum2 = randomNum1+randomNum2;
     res.send(result);
 
     // Update the position in the JSON file
     updatePlayerData('player2.json', randomNum1 + randomNum2);
 });
+
+app.get('/getLastRoll/:player', (req, res) => {
+    const player = req.params.player;
+    if(player===1)
+    res.json({ diceSum: previous_dice_sum1 });
+    else
+    res.json({ diceSum: previous_dice_sum2 });
+})
 
 // Function to update the position in a JSON file
 function updatePlayerData(filename, roll) {
@@ -70,6 +83,75 @@ app.get('/getPlayerData1', (req, res) => {
 app.get('/getPlayerData2', (req, res) => {
     const data = readJsonFile('player2.json');
     res.send({ position: data.position, country: data.country, money: data.money });
+});
+
+app.get('/getPlayerTicketData/:player', (req, res) => {
+    const player = req.params.player;
+    const playerFilePath = path.join(__dirname, `player${player}.json`);
+
+    // Read player data from the JSON file
+    fs.readFile(playerFilePath, 'utf8', (err, playerData) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        const playerJson = JSON.parse(playerData);
+        const cityName = playerJson.country;
+
+        // Read ticket data from the JSON file
+        const ticketFilePath = path.join(__dirname, 'ticketData.json');
+        fs.readFile(ticketFilePath, 'utf8', (err, ticketData) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Internal Server Error');
+                return;
+            }
+
+            const ticketJson = JSON.parse(ticketData);
+
+            // Retrieve ticket info based on the player's current city
+            const ticketInfo = ticketJson[cityName];
+
+            if (ticketInfo) {
+                res.send(ticketInfo);
+            } else {
+                res.status(404).send('Ticket data not found');
+            }
+        });
+    });
+});
+
+app.get('/handleUno/:player/:diceSum', (req, res) => {
+    const player = req.params.player;
+    const diceSum = parseInt(req.params.diceSum, 10);
+
+    if (isNaN(diceSum)) {
+        res.status(400).send('Invalid dice sum');
+        return;
+    }
+
+    // Read ticket data from the JSON file
+    const ticketFilePath = path.join(__dirname, 'ticketData.json');
+    fs.readFile(ticketFilePath, 'utf8', (err, ticketData) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        const ticketJson = JSON.parse(ticketData);
+
+        // Retrieve Uno message based on the dice sum
+        const unoMessage = ticketJson.uno[`Message_${diceSum}`];
+
+        if (unoMessage) {
+            res.send({ message: unoMessage });
+        } else {
+            res.status(404).send('Uno message not found');
+        }
+    });
 });
 
 // Function to read a JSON file
